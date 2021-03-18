3f969d114dfacfd6d130b1234638d1dac8030d62
Natalia Maximo <natalia.maximo@uottawa.ca>
2021-03-17 23:14:29 -0400

    chg: sadder_keanu_reeves.jpg

--- tex-install.sh
+++ tex-install.sh
@@ -2,4 +2,5 @@
 sudo apt-get update;
 sudo apt-get install \
     texlive-latex-base texlive-fonts-recommended texlive-fonts-extra \
-    texlive-latex-extra texlive-pictures texlive-pstricks;
+    texlive-latex-extra texlive-pictures texlive-pstricks \
+    texlive-bibtex-extra biber;
